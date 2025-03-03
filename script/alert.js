aebc065e6f27b28ed80edf3dc9415ad39728866a
sajeeb5523 <sajeebaljabed1@gmail.com>
2025-03-03 12:21:28 +0600

    alert updatd

---
+++ script/alert.js
@@ -0,0 +1,24 @@
+document.getElementById('alert').addEventListener('click', function () {
+    alert('Board updated successfully')
+})
+
+document.getElementById('alert1').addEventListener('click', function () {
+    alert('Board updated successfully')
+})
+
+document.getElementById('alert2').addEventListener('click', function () {
+    alert('Board updated successfully')
+})
+
+document.getElementById('alert3').addEventListener('click', function () {
+    alert('Board updated successfully')
+})
+
+document.getElementById('alert4').addEventListener('click', function () {
+    alert('Board updated successfully')
+})
+
+document.getElementById('alert5').addEventListener('click', function () {
+    alert('Board updated successfully')
+    alert('congrates!!! You have completed all the cureent task')
+})
